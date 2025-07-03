49081f5b5de7228ce689bc6fdeeb0b7c7200d177
guilhermetairaleme <guilhermetairasolutions@gmail.com>
2025-07-03 17:15:42 -0300

    este commit coloca as fotos novas

--- resources/js/components/ApplicationLogo.vue
+++ resources/js/components/ApplicationLogo.vue
@@ -1,8 +1,23 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const subdomain = ref("promptplay"); // valor padrão, caso não haja subdomínio
+
+onMounted(() => {
+  const host = window.location.hostname;
+  const parts = host.split(".");
+
+  if (parts.length >= 2) {
+    subdomain.value = parts[0];
+  }
+});
+</script>
 <template>
   <img
-    src="/imagem/promptplay.png"
+    :src="`/imagem/${subdomain}.promptplay.png`"
     alt="Logo"
-    style="width: 300px; height: auto;"
+    style="width: 150px; height: auto;"
     class="mx-auto transition-transform duration-300 hover:scale-105"
   />
 </template>
+
